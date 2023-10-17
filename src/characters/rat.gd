ef9981245a1e79e2db9cad05dbb5201ab545c884
Jerry9099 <ebooks2222@gmail.com>
2023-10-17 00:38:07 -0700

    added rat

--- src/characters/rat.gd
+++ src/characters/rat.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-@export var speed = 150.0
-@export var health = 100
+@export var speed = 300.0
+@export var health = 50
 @export var projectile_scene: PackedScene
 @export var projectile_speed = 300
 
@@ -24,10 +24,3 @@ func take_damage(damage):
 	health = health - damage
 	if health <=0:
 		queue_free()
-
-func _on_attack_timer_timeout():
-	if get_node("../player"):
-		var projectile = projectile_scene.instantiate()
-		projectile.position = position #+ Vector2(-60, -10)
-		projectile.velocity = Vector2(0, 0).move_toward(player.global_position + player.velocity/3.5 - global_position, projectile_speed).normalized() * projectile_speed
-		get_tree().get_root().add_child(projectile)
